72a708053ac99865c1eb721d3868297b97a91215
“yuwen” <yuwen@kth.se>
2020-11-27 12:05:18 +0100

    responsive fonts

--- src/Result.js
+++ src/Result.js
@@ -7,11 +7,15 @@ Ball.Result.prototype = {
         //this.add.sprite(0, 0, 'screen-bg-passed');
         this.passInfo = this.add.text(Ball._WIDTH*0.5, Ball._HEIGHT*1.0/3.0, 'Passed Test', style);
         this.passInfo.anchor.setTo(0.5);
+        this.passInfo.scale.setTo(Ball.scaleFactor);
+        
         this.timeInfo = this.add.text(Ball._WIDTH*0.5, Ball._HEIGHT*(1.0/3.0+1.0/12.0), 'Time: ' + this.game.state.states['Game'].timer + 's', Ball.fontSmall);
         this.timeInfo.anchor.setTo(0.5);
+        this.timeInfo.scale.setTo(Ball.scaleFactor);
         this.collisionInfo = this.add.text(Ball._WIDTH*0.5, Ball._HEIGHT*(1.0/3.0+1.0/12.0+1.0/25.0), 
             'Total collisions: ' + this.game.state.states['Game'].totalCollisions, Ball.fontSmall);
         this.collisionInfo.anchor.setTo(0.5);
+        this.collisionInfo.scale.setTo(Ball.scaleFactor);
         //this.game.state.states['Game'].timer
 	}
 };
